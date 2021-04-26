baaeab1a44d1de8f158e06aa577b5d5c2b840be1
HLCaptain <pkblazsak@gmail.com>
2021-04-26 23:15:52 +0200

    Code cleanup and input/output fixes

--- src/PageHandlingSimulator.java
+++ src/PageHandlingSimulator.java
@@ -3,22 +3,24 @@ import java.util.Vector;
 
 public class PageHandlingSimulator {
 	private final LRU lru;
-	private int pageFault;
 
+	/**
+	 * Constructor initializes 3 pages: A, B, C<br>
+	 * The LRU algorithm works with max 3 unit page locks.
+	 */
 	public PageHandlingSimulator() {
 		lru = new LRU(3);
 		Vector<Page> pages = new Vector<>();
-		Page page = new Page("A");
+		Page page = new Page("C");
 		page.setData(0);
 		pages.add(page);
 		page = new Page("B");
 		page.setData(0);
 		pages.add(page);
-		page = new Page("C");
+		page = new Page("A");
 		page.setData(0);
 		pages.add(page);
 		lru.setPages(pages);
-		this.pageFault = 0;
 	}
 
 	/**
@@ -26,7 +28,12 @@ public class PageHandlingSimulator {
 	 * @return references of numbers.
 	 */
 	public Vector<Integer> getInput() {
-		String line = new Scanner(System.in).nextLine();
+		Scanner scanner = new Scanner(System.in);
+		String line;
+		if (scanner.hasNext()) {
+			line = scanner.nextLine();
+		} else return new Vector<>(); // If we did not read in anything, return with empty array.
+		//while (scanner.hasNext()); // useless line, need to read all of the inputs.
 		Vector<Integer> pageReferences = new Vector<>();
 		for (String str : line.split(",")) {
 			pageReferences.add(Integer.parseInt(str));
@@ -45,8 +52,15 @@ public class PageHandlingSimulator {
 		computeInputs(pageReferences);
 	}
 
+	/**
+	 * Handles inputs and outputs the solution.
+	 * @param inputs references of the data (numbers)
+	 */
 	public void computeInputs(Vector<Integer> inputs) {
+		int pageFault = 0; // counting failed data references.
 		StringBuilder output = new StringBuilder();
+
+		// Reacting to every input.
 		for (Integer input : inputs) {
 			String nextStep = lru.pageCall(input);
 			// If not able to read in data, it failed.
@@ -55,6 +69,9 @@ public class PageHandlingSimulator {
 			}
 			output.append(nextStep);
 		}
+
+		// Output
 		System.out.println(output);
+		System.out.println(pageFault);
 	}
 }
